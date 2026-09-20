706d946a972f11b7b5abbaca099b84284c36dde1
jirafadon <jirafadon@gmail.com>
2026-09-20 15:17:34 -0300

    security: agregar sanitización básica de HTML

---
+++ backend/utils/sanitize.js
@@ -0,0 +1,7 @@
+export function stripHtml(value = '') {
+  return String(value ?? '')
+    .replace(/<[^>]*>/g, ' ')
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, '')
+    .replace(/\s+/g, ' ')
+    .trim();
+}
