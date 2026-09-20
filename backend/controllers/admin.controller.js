a7c2cf2aeb04138874615022302c5ac4242b827e
jirafadon <jirafadon@gmail.com>
2026-09-20 15:19:17 -0300

    feat(cupones): inicializar cupones predeterminados en bootstrap

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -20,6 +20,7 @@ async function ensureDefaultCoupons() {
 
 export async function getBootstrap(req, res, next) {
   try {
+    await ensureDefaultCoupons();
     await Product.updateMany(
       { image: { $exists: true, $ne: '' }, $or: [{ images: { $exists: false } }, { images: { $size: 0 } }] },
       [{ $set: { images: ['$image'] } }]
